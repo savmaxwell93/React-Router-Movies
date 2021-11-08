5a852746f00aa5e689456ad9fb5b6d8e5b402648
Savannah <savmaxwell@gmail.com>
2021-11-08 13:58:11 -0700

    completed project setup and route setup

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function MovieList(props) {
+  const { movies } = props
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
